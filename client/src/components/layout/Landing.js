e4b5af0fd6b7af12fed46eec5c645b6359f13a47
mmatez <mohamed.moaetez@live.fr>
2018-09-05 12:12:22 +0100

    mriguel

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { PropTypes } from 'prop-types';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { PropTypes } from "prop-types";
+import { connect } from "react-redux";
 
 class Landing extends Component {
   componentDidMount() {
     if (this.props.auth.isAuthenticated) {
-      this.props.history.push('/dashboard');
+      this.props.history.push("/dashboard");
     }
   }
 
@@ -17,12 +17,8 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Developer Connector</h1>
-                <p className="lead">
-                  {' '}
-                  Create a developer profile/portfolio, share posts and get help
-                  from other developers
-                </p>
+                <h1 className="display-3 mb-4"> Search,Compare & Save</h1>
+                <p className="lead"> Big Brands,Huge Choice,Massive Savings!</p>
                 <hr />
                 <Link to="/register" className="btn btn-lg btn-info mr-2">
                   Sign Up
